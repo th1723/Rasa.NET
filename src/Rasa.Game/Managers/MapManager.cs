65afff847abab6dd5cc815044d73721b9a11bf49
Blumster <Blumster@users.noreply.github.com>
2020-04-26 13:12:53 +0200

    Merge dev-master-enterworld to master to be able to enter world (#52)
    * Add a partially working character selection implementation
    
    * Rework database tables and add sql scripts
    
    * Fix typo in the SQL script
    
    * Drop unnecessary code and rename a field for readability
    
    * Fix a false-positive error message for nested classes in the database handling
    
    * Add default user/password for easier db handling across different dev pcs
    
    * Fix error for older MySQL servers
    
    * Fix typos
    
    * Use specific packet classes to shorten packet implementation files
    
    * Implement character deletion
    
    * Implement DeleteCharacterFailedPacket
    
    * Implement family name checking
    
    * Update console projects to .NET Core 3.0 preview
    
    * Implement protocol PingMessage
    
    * Small fixes
    
    * continued work on master character selection (#46)
    
    * Added ReadUnkStruct function to PythonReader
    * Fixed PythonReader.ToString() function
    * Characters got face and hair
    
    * Ready to enter world (#47)
    
    * Added default position and appearance to character creation
    * Character Info at character selection now display all data
    * Ready to enter world
    
    * In game again (#48)
    
    * In game again
    * Added missing packets, client don't disconnect upon entering world
    * Fixed naming of 2 packets
    
    * Update CharacterTable.cs
    
    * Character got new look
    
    Co-authored-by: krssrb <ivankohodolic@gmail.com>

---
+++ src/Rasa.Game/Managers/MapManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace Rasa.Managers
+{
+    using Data;
+    using Database.Tables.Character;
+    using Game;
+    using Packets;
+    using Packets.Game.Server;
+    using Structures;
+    using Timer;
+
+    public class MapManager
+    {
+
+        private static MapManager _instance;
+        private static readonly object InstanceLock = new object();
+        private readonly Timer Timer = new Timer();
+
+        public static MapManager Instance
+        {
+            get
+            {
+                // ReSharper disable once InvertIf
+                if (_instance == null)
+                {
+                    lock (InstanceLock)
+                    {
+                        if (_instance == null)
+                            _instance = new MapManager();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        private MapManager()
+        {
+        }
+
+        internal void MapLoaded(Client client)
+        {
+            var player = client.MapClient.Player;
+
+            // default enetiyId, until we implement EntityManager
+            var entityId = 10001u;
+
+            var entityData = new List<PythonPacket>
+            {
+                new WorldLocationDescriptorPacket(new Vector3((float)player.CoordX, (float)player.CoordY, (float)player.CoordZ), (float)player.Rotation),
+                new AppearanceDataPacket(CharacterAppearanceTable.GetAppearances(player.Id))
+            };
+
+            var map = new Map
+            {
+                MapInfo = new MapInfo(1220, "adv_foreas_concordia_wilderness", 1556, 0)
+            };
+
+            client.CallMethod(SysEntity.ClientMethodId, new CreatePhysicalEntityPacket(entityId, (EntityClass)player.Gender == 0 ? (EntityClass)692 : (EntityClass)691, entityData));
+            client.CallMethod(SysEntity.ClientMethodId, new SetCurrentContextIdPacket(map.MapInfo.ContextId));
+            client.CallMethod(SysEntity.ClientMethodId, new SetControlledActorIdPacket(entityId));
+
+            client.State = ClientState.Ingame;
+        }
+
+        internal void PassClientToMap(Client client)
+        {
+            var map = new Map
+            {
+                MapInfo = new MapInfo(1220, "adv_foreas_concordia_wilderness", 1556, 0)
+            };
+
+            client.CallMethod(SysEntity.ClientMethodId, new PreWonkavatePacket());
+            client.CallMethod(SysEntity.CurrentInputStateId, new WonkavatePacket
+               (
+                   map.MapInfo.ContextId,
+                   map.InstanceId,
+                   map.MapInfo.Version,
+                   new Vector3((float)client.MapClient.Player.CoordX, (float)client.MapClient.Player.CoordY, (float)client.MapClient.Player.CoordZ),
+                   (float)client.MapClient.Player.Rotation
+               ));
+
+            client.State = ClientState.Loading;
+        }
+
+        internal void Ping(Client client, double ping)
+        {
+            client.CallMethod(SysEntity.ClientMethodId, new AckPingPacket(ping));
+        }
+    }
+}
